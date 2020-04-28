3a8ae234b4b957e1d1b4430b1f39a1bd47dee938
diop <fodesdiop@gmail.com>
2020-04-28 23:54:55 +0000

    Implement predict method

--- genclf/genclf/genclf.py
+++ genclf/genclf/genclf.py
@@ -1,6 +1,10 @@
 # Core packages
 
 import os, joblib
+import warnings 
+warnings.filterwarnings('ignore')
+
+PACKAGE_DIR = os.path.dirname(__file__)
 
 class GenderClassifier(object):
     """genderClassifier"""
@@ -10,3 +14,24 @@ class GenderClassifier(object):
 
     def __repr__(self):
         return f'GenderClassifier(name={self.name})'
+
+    def predict(self):
+        # Load vectorizer
+        gender_vectorizer = open(os.path.join(PACKAGE_DIR, 'models/gender_vectorizer.pkl'), 'rb')
+        gender_cv = joblib.load(gender_vectorizer)
+
+        # Load Models
+        gender_nb_model = open(os.path.join(PACKAGE_DIR, 'models/gender_naive_bayes_model.pkl'), 'rb')
+        gender_clf = joblib.load(gender_nb_model)
+        
+        # Vectorization
+        vectorized_data = gender_cv.transform([self.name]).toarray()
+        prediction = gender_clf.predict(vectorized_data)
+
+        if prediction[0] == 0:
+            result = 'female'
+        elif prediction[0] == 1:
+            result = 'male'
+
+        return result
+
