f3cc6908f1b0efa1462be558c6e71984b655466d
diop <fodesdiop@gmail.com>
2020-04-29 16:59:26 +0000

    Update env template

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+import dotenv
 import uvicorn
-from fastapi import FastAPI
-
-# Machine Learning Package
 import os, joblib
+from fastapi import FastAPI, Form, Response
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+
+dotenv.load_dotenv('.env')
+
+acccount_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+twilio_client = Client(acccount_sid, auth_token)
 
 # Vectorizer
 gender_vectorizer = open('genclf/genclf/models/gender_vectorizer.pkl', 'rb')
@@ -23,6 +30,8 @@ async def index():
 async def get_names(name):
     return {'Name': name}
 
+
+# Machine Learning Prediction Routes
 @app.get('/predict/{name}')
 async def predict(name):
     vectorized_name = gender_cv.transform([name]).toarray()
@@ -47,6 +56,25 @@ async def predict(name):
 
     return {"Given name": name, "Prediction": result}
 
+@app.post('/hook')
+async def process_sms(From: str = Form(...), Body: str = Form(...)):
+    response = MessagingResponse()
+
+    print(Body)
+
+    vectorized_name = gender_cv.transform([Body]).toarray()
+    prediction = gender_clf.predict(vectorized_name)
+    if prediction[0] == 0:
+        result = 'Female'
+    else:
+        result = 'Male'
+
+    response.message(result)
+
+    print(response)
+
+    return Response(content=str(response), media_type="application/xml")
+    
 
 if __name__ == '__main__':
     uvicorn.run(app,host='127.0.0.1',port=8000)
